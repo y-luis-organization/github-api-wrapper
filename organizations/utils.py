b70ff20acf33602195367c6fff431c41a0e3d561
y-luis <j.luis@coodriver.com>
2017-11-27 01:09:19 +0000

    Follow next link header breaks because of rate limits

--- organizations/utils.py
+++ organizations/utils.py
@@ -1,4 +1,9 @@
 def get_page_biggest_repo(repos):
+    """
+    Get biggest repo
+    :param repos: List of JSON repos
+    :return: Tuple with repo name and repo size
+    """
     biggest_repo = ('', 0)
 
     for repo in repos:
@@ -6,3 +11,12 @@ def get_page_biggest_repo(repos):
             biggest_repo = (repo['name'], repo['size'])
 
     return biggest_repo
+
+
+def get_links(link_header):
+    """
+    Parse Link header
+    :return: Links as a dictionary
+    """
+    return {y[1].split('=')[1].replace('\"', ''): y[0].replace('<', '').replace('>', '') for y in
+             [x.split('; ') for x in link_header.split(', ')]}
--- organizations/views.py
+++ organizations/views.py
@@ -1,7 +1,7 @@
 import requests
-from django.http import JsonResponse, HttpResponseForbidden
+from django.http import JsonResponse
 
-from organizations.utils import get_page_biggest_repo
+from organizations.utils import get_page_biggest_repo, get_links
 
 ACCEPT_HEADER = 'application/vnd.github.v3+json'
 
@@ -11,13 +11,32 @@ PER_PAGE = 100
 
 def get_organizations(request):
     organizations_response = requests.get('https://api.github.com/organizations?per_page={0}'.format(PER_PAGE),
-                                         headers={'Accept': ACCEPT_HEADER})
+                                          headers={'Accept': ACCEPT_HEADER})
 
     organizations = organizations_response.json()
 
-    response = {'organizations': len(organizations)}
+    count = len(organizations)
 
-    pass
+    link_header = organizations_response.headers.get('Link')
+
+    # Follow links
+    while link_header is not None:
+        links = get_links(link_header)
+
+        if links.get('next') is not None:
+            next = requests.get(links.get('next'), headers={'Accept': ACCEPT_HEADER})
+
+            # API rate limit exceeded
+            if next.status_code == 403:
+                return JsonResponse(next.content, status=403)
+
+            count += len(next.json())
+
+            link_header = next.headers.get('Link')
+
+    response = {'organizations': count}
+
+    return JsonResponse(response)
 
 
 def get_repos(request, organization_name):
@@ -40,18 +59,17 @@ def get_repos(request, organization_name):
 
     link_header = repos_response.headers.get('Link')
 
+    # Follow links
     if link_header is not None:
 
-        # Parse Link header as a dictionary
-        links = {y[1].split('=')[1].replace('\"', ''): y[0].replace('<', '').replace('>', '') for y in
-                 [x.split('; ') for x in link_header.split(', ')]}
+        links = get_links(link_header)
 
         if links.get('next') is not None:
             next = requests.get(links.get('next'), headers={'Accept': ACCEPT_HEADER})
 
             # API rate limit exceeded
             if next.status_code == 403:
-                return HttpResponseForbidden(next.content)
+                return JsonResponse(next.content, status=403)
 
             next_biggest_repo = get_page_biggest_repo(next.json())
 
